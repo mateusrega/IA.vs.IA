46fd907fb9312871cd9e0bef919d0cf1d7005dd1
mateusrega <143010456+mateusrega@users.noreply.github.com>
2026-01-07 00:45:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+import { createGrid } from "./core/grid.js";
+import { seedGrid } from "./core/init.js";
 import { renderGrid } from "./render/canvas.js";
 import { updateHUD } from "./render/hud.js";
+
+const grid = createGrid();
+seedGrid(grid);
 
 let turn = 0;
 
 function loop() {
   turn++;
-  renderGrid();
-  updateHUD(turn, "Simulação visual");
 
-  setTimeout(loop, 500);
+  // 🔹 Por enquanto só render
+  renderGrid(grid);
+  updateHUD(turn, "Grid real ativo");
+
+  setTimeout(loop, 400);
 }
 
 loop();
